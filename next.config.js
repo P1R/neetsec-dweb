4a6e4b5a3888e0cd0be6919489726c136596e749
P1R0 <daroperez_negron@esimez.mx>
2020-11-14 01:35:07 -0600

    Updated to default for Dweb

--- next.config.js
+++ next.config.js
@@ -1,41 +1,25 @@
-//const withSass = require('@zeit/next-sass')     
-//module.exports = {
-//  exportTrailingSlash: true,
-//  exportPathMap: function() {
-//    return {
-//      '/': { page: '/' }
-//    };
-//  },
-//  withSass({
-//      webpack: function (config) {
-//        config.module.rules.push({
-//          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-//          use: {
-//            loader: 'url-loader',
-//            options: {
-//              limit: 100000,
-//              name: '[name].[ext]'
-//            }
-//          }
-//        })
-//        return config
-//      }
-//    })
-//};
+module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  },
+};
 
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    webpack: function (config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            name: '[name].[ext]'
-          }
-        }
-      })
-      return config
-    }
-  })
+//const withSass = require('@zeit/next-sass')
+//module.exports = withSass({
+//    webpack: function (config) {
+//      config.module.rules.push({
+//        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+//        use: {
+//          loader: 'url-loader',
+//          options: {
+//            limit: 100000,
+//            name: '[name].[ext]'
+//          }
+//        }
+//      })
+//      return config
+//    }
+//  })
