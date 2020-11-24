32f7b513c2768dd8df0b729accfa8bf54018e9b3
P1R0 <daroperez_negron@esimez.mx>
2020-11-23 20:13:21 -0600

    Update next.config.js
    test new

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-//module.exports = {
-//  exportTrailingSlash: true,
-//  exportPathMap: function() {
-//    return {
-//      '/': { page: '/' }
-//    };
-//  },
-//};
-
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    webpack: function (config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            name: '[name].[ext]'
-          }
-        }
-      })
-      return config
-    }
-  })
+module.exports = {
+  assetPrefix: './',
+  exportTrailingSlash: true,
+}
